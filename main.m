0289985d6561247edc0618818f2cef34e2e375a4
Emma Wawrzynek <efwawrzynek@berkeley.edu>
2024-04-15 20:06:42 -0400

    mac backslashes

--- main.m
+++ main.m
@@ -1,13 +1,21 @@
 %% Read and plot original data tt
 clear all 
 
-r01_data = "data\r01.edf"; 
+r01_data = "data/r01.edf"; 
 
-[data_r01,anno_r01,info_r01,fs_r01] = read_edf("data\r01.edf");
-[data_r04,anno_r04,info_r04,fs_r04] = read_edf("data\r04.edf");
-[data_r07,anno_r07,info_r07,fs_r07] = read_edf("data\r07.edf");
-[data_r08,anno_r08,info_r08,fs_r08] = read_edf("data\r08.edf");
-[data_r10,anno_r10,info_r10,fs_r10] = read_edf("data\r10.edf");
+[data_r01,anno_r01,info_r01,fs_r01] = read_edf("data/r01.edf");
+[data_r04,anno_r04,info_r04,fs_r04] = read_edf("data/r04.edf");
+[data_r07,anno_r07,info_r07,fs_r07] = read_edf("data/r07.edf");
+[data_r08,anno_r08,info_r08,fs_r08] = read_edf("data/r08.edf");
+[data_r10,anno_r10,info_r10,fs_r10] = read_edf("data/r10.edf");
+
+% r01_data = "data\r01.edf"; 
+% 
+% [data_r01,anno_r01,info_r01,fs_r01] = read_edf("data\r01.edf");
+% [data_r04,anno_r04,info_r04,fs_r04] = read_edf("data\r04.edf");
+% [data_r07,anno_r07,info_r07,fs_r07] = read_edf("data\r07.edf");
+% [data_r08,anno_r08,info_r08,fs_r08] = read_edf("data\r08.edf");
+% [data_r10,anno_r10,info_r10,fs_r10] = read_edf("data\r10.edf");
 
 
 %%
