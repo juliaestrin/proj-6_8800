84e9e014291bc5c1af7aa58caea68062097060e3
juliaestrin <juliaestrin3@gmail.com>
2024-05-01 21:20:13 -0400

    Adding lab ica code

--- main.m
+++ main.m
@@ -1,13 +1,12 @@
-%% Read and plot original data tt
+%% Read and plot original data
 clear all 
-
 
 % Read data for PC 
 [data_r01,anno_r01,info_r01,fs_r01] = read_edf("data/r01.edf");
-[data_r04,anno_r04,info_r04,fs_r04] = read_edf("data/r04.edf");
-[data_r07,anno_r07,info_r07,fs_r07] = read_edf("data/r07.edf");
-[data_r08,anno_r08,info_r08,fs_r08] = read_edf("data/r08.edf");
-[data_r10,anno_r10,info_r10,fs_r10] = read_edf("data/r10.edf");
+% [data_r04,anno_r04,info_r04,fs_r04] = read_edf("data/r04.edf");
+% [data_r07,anno_r07,info_r07,fs_r07] = read_edf("data/r07.edf");
+% [data_r08,anno_r08,info_r08,fs_r08] = read_edf("data/r08.edf");
+% [data_r10,anno_r10,info_r10,fs_r10] = read_edf("data/r10.edf");
 
 % Read data for MAC 
 % [data_r01,anno_r01,info_r01,fs_r01] = read_edf("data\r01.edf");
@@ -30,11 +29,11 @@ plot(t_r01_1,ecg_r01_1)
 hold on 
 scatter(qrs_t_r01_1,qrs_r01_1)
 xlabel('time (s)'); 
-ylabel('ECG Amplitude (mv??)')
+ylabel('ECG Amplitude')
 title('Original Invasive ECG from r01')
 
 
-%% Band pass filter the signal from r01 
+%% Filter and plot r01 data
 t1 = 1; 
 t2 = 5; 
 
@@ -43,7 +42,7 @@ fh = 30;
 fs = fs_r01(signum); 
 N = 1024; 
 
-r01_1_filtered = bandpassfilter(ecg_r01_1,fl,fh,fs,N); 
+r01_1_filtered = bandpassfilter(ecg_r01_1,t_r01_1,fl,fh,fs,N); 
 
 [chunck_t_r01_1,chunk_r01_1_filtered,t_qrs_r01_1,qrs_r01_1] = chunk_ecg(t1,t2,t_r01_1,r01_1_filtered,qrs_t_r01_1); 
 
@@ -52,28 +51,28 @@ signum = 2;
 [t_r01_2,ecg_r01_2] = extract_edf(signum,data_r01,info_r01,fs_r01); 
 qrs_t_r01_2 = seconds(anno_r01.Onset); 
 % qrs_r01_2 = find_qrs(qrs_t_r01_2,t_r01_2,ecg_r01_2);
-r01_2_filtered = bandpassfilter(ecg_r01_2,fl,fh,fs,N); 
+r01_2_filtered = bandpassfilter(ecg_r01_2,t_r01_2, fl,fh,fs,N); 
 [chunck_t_r01_2,chunk_r01_2_filtered,t_qrs_r01_2,qrs_r01_2] = chunk_ecg(t1,t2,t_r01_2,r01_2_filtered,qrs_t_r01_2); 
 
 signum = 3;
 [t_r01_3,ecg_r01_3] = extract_edf(signum,data_r01,info_r01,fs_r01); 
 qrs_t_r01_3 = seconds(anno_r01.Onset); 
 % qrs_r01_3 = find_qrs(qrs_t_r01_3,t_r01_3,ecg_r01_3);
-r01_3_filtered = bandpassfilter(ecg_r01_3,fl,fh,fs,N); 
+r01_3_filtered = bandpassfilter(ecg_r01_3,t_r01_3,fl,fh,fs,N); 
 [chunck_t_r01_3,chunk_r01_3_filtered,t_qrs_r01_3,qrs_r01_3] = chunk_ecg(t1,t2,t_r01_3,r01_3_filtered,qrs_t_r01_3); 
 
 signum = 4;
 [t_r01_4,ecg_r01_4] = extract_edf(signum,data_r01,info_r01,fs_r01); 
 qrs_t_r01_4 = seconds(anno_r01.Onset); 
 % qrs_r01_4 = find_qrs(qrs_t_r01_4,t_r01_4,ecg_r01_4);
-r01_4_filtered = bandpassfilter(ecg_r01_4,fl,fh,fs,N); 
+r01_4_filtered = bandpassfilter(ecg_r01_4,t_r01_4,fl,fh,fs,N); 
 [chunck_t_r01_4,chunk_r01_4_filtered,t_qrs_r01_4,qrs_r01_4] = chunk_ecg(t1,t2,t_r01_4,r01_4_filtered,qrs_t_r01_4); 
 
 signum = 5;
 [t_r01_5,ecg_r01_5] = extract_edf(signum,data_r01,info_r01,fs_r01); 
 qrs_t_r01_5 = seconds(anno_r01.Onset); 
 % qrs_r01_5 = find_qrs(qrs_t_r01_5,t_r01_5,ecg_r01_5);
-r01_5_filtered = bandpassfilter(ecg_r01_5,fl,fh,fs,N); 
+r01_5_filtered = bandpassfilter(ecg_r01_5,t_r01_5,fl,fh,fs,N); 
 [chunck_t_r01_5,chunk_r01_5_filtered,t_qrs_r01_5,qrs_r01_5] = chunk_ecg(t1,t2,t_r01_5,r01_5_filtered,qrs_t_r01_5); 
 
 
@@ -105,3 +104,9 @@ xlabel('time (s)');
 ylabel('ECG Amplitude')
 legend('Materal ECG 2','Materal ECG 3','Materal ECG 4','Fetal QRS Complex Annotation')
 
+
+%% Try lab ICA algorithm 
+X = [qrs_r01_3,qrs_r01_4,qrs_r01_5]; 
+[W, Zhat] = ica(X);
+Z = W.*X'; 
+Z = Z'; 
